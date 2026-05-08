9e0555319e2152fa26c7496dec4605367442ebef
Evgenii O <jefo.work@gmail.com>
2026-05-08 20:52:25 +0700

    case study 1

--- webapp/astro.config.mjs
+++ webapp/astro.config.mjs
@@ -6,6 +6,8 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kaizin-digital.github.io',
+  base: '/kaizin-systems-wesite',
   vite: {
     plugins: [tailwindcss()]
   },
